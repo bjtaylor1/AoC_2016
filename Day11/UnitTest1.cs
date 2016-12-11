909b2edbe1e1823810be1fb4016f39e3b08c49e5
Ben Taylor <ben.taylor@ssp-uk.com>
2016-12-11 11:37:39 +0000

    day 11

--- Day11/UnitTest1.cs
+++ Day11/UnitTest1.cs
@@ -25,6 +25,140 @@ namespace Day11
                 });
         }
 
+        [Test]
+        public void TestEquality()
+        {
+            var state1 = new State
+            {
+                Floors = new []
+                {
+                    new List<string> {"AB", "AC", "AD"}, 
+                    new List<string> {"AC", "AD"}, 
+                    new List<string> {"AD"}, 
+                    new List<string> {"AD", "AG", "AH"}
+                },
+                Pos = 1
+            };
+            var state2 = new State
+            {
+                Floors = new []
+                {
+                    new List<string> {"AB", "AC", "AD"}, 
+                    new List<string> {"AC", "AD"}, 
+                    new List<string> {"AD"}, 
+                    new List<string> {"AD", "AG", "AH"}
+                },
+                Pos = 1
+            };
+            state1.Equals(state2).Should().BeTrue();
+        }
+
+        [Test]
+        public void TestInequality()
+        {
+            var state1 = new State
+            {
+                Floors = new []
+                {
+                    new List<string> {"AB", "AC", "AD"}, 
+                    new List<string> {"AC", "AD"}, 
+                    new List<string> {"AD"}, 
+                    new List<string> {"AD", "AG"}
+                },
+                Pos = 1
+            };
+            var state2 = new State
+            {
+                Floors = new []
+                {
+                    new List<string> {"AB", "AC", "AD"}, 
+                    new List<string> {"AC", "AD"}, 
+                    new List<string> {"AD"}, 
+                    new List<string> {"AD", "AG", "AH"}
+                },
+                Pos = 1
+            };
+            state1.Equals(state2).Should().BeFalse();
+        }
+
+        [Test]
+        public void Advance()
+        {
+            var state = new State { Floors = new[]
+            {
+                new List<string> { "AB", "AC", "AD" },
+                new List<string>(), 
+                new List<string>(), 
+                new List<string>()
+            } };
+            var newState = state.ApplyMove(new[] {"AB", "AC"}, 1);
+            newState.Floors[1].Should().HaveCount(2).And.Contain(new[] {"AB", "AC"});
+            newState.Floors[0].Should().HaveCount(1).And.Contain(new[] {"AD"});
+            newState.Pos.Should().Be(1);
+        }
+
+        [Test]
+        public void Sort()
+        {
+            var goodState = new State
+            {
+                Floors = new[]
+                {
+                    new List<string>(),
+                    new List<string>(),
+                    new List<string> {"AB", "AC", "AD"}, /* they're higher up */
+                    new List<string>()
+                }
+            };
+            var badState = new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {"AB", "AC", "AD"},
+                    new List<string>(),
+                    new List<string>(),
+                    new List<string>()
+                }
+            };
+            var states = new List<State> { badState, goodState};
+            states.Sort();
+            states.First().Should().Be(goodState);
+        }
+
+
+        [Test]
+        public void Example()
+        {
+            var initialState = new State
+            {
+                Floors = new []
+                {
+                    new List<string> {"HyM", "LiM"}, 
+                    new List<string> {"HyG"}, 
+                    new List<string> {"LiG"},
+                    new List<string>(), 
+                }
+            };
+
+            var allStates = new List<State> {initialState};
+            State completeState = null;
+            while (completeState == null)
+            {
+                var allNextStates = new List<State>();
+                foreach (var state in allStates)
+                {
+                    var nextStates = state.GetTransitions().Where(s => s.IsValid() && !allStates.Contains(s)).ToArray();
+                    completeState = nextStates.FirstOrDefault(s => s.IsComplete());
+                    if (completeState != null) break;
+                    allNextStates.AddRange(nextStates);
+                }
+                allStates.AddRange(allNextStates);
+                allStates.Sort();
+                allStates = allStates.Take((int) 1e6).ToList();
+            }
+            Console.Out.WriteLine(completeState.Depth);
+        }
+
         [TestCase(false, "THG", "LIM")]
         [TestCase(false, "THG", "LIM", "PRG", "PRM")]
         [TestCase(true, "THG", "LIG", "LIM")]
@@ -38,31 +172,235 @@ namespace Day11
             state.IsValid().Should().Be(isValid);
         }
 
+        [Test]
+        public void ExampleResult()
+        {
+            var state = new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {"HyM", "LiM"},
+                    new List<string> {"HyG"},
+                    new List<string> {"LiG"},
+                    new List<string>()
+                }
+            };
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {"LiM"},
+                    new List<string> {"HyG", "HyM"},
+                    new List<string> {"LiG"},
+                    new List<string>()
+                },
+                Pos = 1
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {"LiM"},
+                    new List<string> {},
+                    new List<string> { "HyG", "HyM", "LiG" },
+                    new List<string>()
+                },
+                Pos = 2
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {"LiM"},
+                    new List<string> {"HyM"},
+                    new List<string> { "HyG",  "LiG" },
+                    new List<string>()
+                },
+                Pos = 1
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> { "HyM", "LiM" },
+                    new List<string> {},
+                    new List<string> { "HyG",  "LiG" },
+                    new List<string>()
+                },
+                Pos = 0
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> { "HyM", "LiM"},
+                    new List<string> { "HyG",  "LiG" },
+                    new List<string>()
+                },
+                Pos = 1
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> {},
+                    new List<string> { "HyG", "HyM" ,  "LiG", "LiM"},
+                    new List<string>()
+                },
+                Pos = 2
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> {},
+                    new List<string> { "HyG",  "LiG"  },
+                    new List<string> { "HyM", "LiM"}
+                },
+                Pos = 3
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> {},
+                    new List<string> { "HyG", "HyM",  "LiG"  },
+                    new List<string> { "LiM"}
+                },
+                Pos = 2
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> {},
+                    new List<string> { "HyM"  },
+                    new List<string> { "HyG",  "LiG", "LiM"}
+                },
+                Pos = 3
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> {},
+                    new List<string> { "HyM", "LiM"  },
+                    new List<string> { "HyG",  "LiG"}
+                },
+                Pos = 2
+            });
+            state = CheckAndAdvance(state, new State
+            {
+                Floors = new[]
+                {
+                    new List<string> {},
+                    new List<string> {},
+                    new List<string> { },
+                    new List<string> { "HyG", "HyM",  "LiG", "LiM" }
+                },
+                Pos = 3
+            });
+            state.IsComplete().Should().BeTrue();
+            state.Depth.Should().Be(11);
+        }
 
-        
+        private static State CheckAndAdvance(State s, State newState)
+        {
+            var transitions = s.GetTransitions();
+            var result = transitions.FirstOrDefault(i => i.Equals(newState));
+            result.Should().NotBeNull();
+            Console.Out.WriteLine(result.GetScore());
+            return result;
+        }
+
+
     }
 
-    public class State
+    public class State : IEquatable<State>, IComparable<State>
     {
+        public bool Equals(State other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            if (Pos != other.Pos) return false;
+            var floorsEqual = Floors[0].SequenceEqual(other.Floors[0])
+                && Floors[1].SequenceEqual(other.Floors[1])
+                && Floors[2].SequenceEqual(other.Floors[2])
+                && Floors[3].SequenceEqual(other.Floors[3]);
+            return floorsEqual;
+        }
+
+        public int CompareTo(State other)
+        {
+            return GetScore().CompareTo(other.GetScore());
+        }
+
+        public int GetScore()
+        {
+            //low == good
+            var topHeaviness = Floors[3].Count*4 +
+                               Floors[2].Count*3 +
+                               Floors[1].Count*2 +
+                               Floors[0].Count;
+            return -topHeaviness;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((State) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return ((Floors != null ? Floors.GetHashCode() : 0)*397) ^ Pos;
+            }
+        }
+
+        public static bool operator ==(State left, State right)
+        {
+            return Equals(left, right);
+        }
+
+        public static bool operator !=(State left, State right)
+        {
+            return !Equals(left, right);
+        }
+
         public List<string>[] Floors = new List<string>[3];
         public int Pos = 0;
-        private State state;
+        public int Depth; /* how many moves it takes to get to this stage */
 
         public State()
         {
         }
 
-        public State(State state, int movement)
+        public State(State state, int movement, int depth)
         {
-            Floors = state.Floors.ToArray();
+            Floors = state.Floors.Select(s => new List<string>(s)).ToArray();
             Pos = state.Pos + movement;
+            Depth = depth;
         }
 
         public bool IsValid()
         {
             var isValid = Floors.All(ContentsCompatible);
             return isValid;
+        }
 
+        public bool IsComplete()
+        {
+            return Floors[0].Count + Floors[1].Count + Floors[2].Count == 0;
         }
 
         private static bool ContentsCompatible(List<string> items)
@@ -100,9 +438,10 @@ namespace Day11
 
         public State ApplyMove(string[] items, int floorChange)
         {
-            var newState = new State(this, floorChange);
+            var newState = new State(this, floorChange, Depth + 1);
             newState.Floors[Pos].RemoveAll(items.Contains);
             newState.Floors[Pos + floorChange].AddRange(items);
+            newState.Floors[Pos + floorChange].Sort();
             return newState;
         }
 
@@ -112,11 +451,11 @@ namespace Day11
             var movableCombinations = GetMovableCombinations();
             if (Pos > 0)
             {
-                states.AddRange(movableCombinations.Select(s => ApplyMove(s, Pos - 1)));
+                states.AddRange(movableCombinations.Select(s => ApplyMove(s, - 1)));
             }
             if (Pos < 3)
             {
-                states.AddRange(movableCombinations.Select(s => ApplyMove(s, Pos + 1)));
+                states.AddRange(movableCombinations.Select(s => ApplyMove(s, 1)));
             }
             return states.ToArray();
         }
